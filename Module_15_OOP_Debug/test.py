837328455c860f9c9d7c5e47c5efd1db1a04a4f8
Walde_r <washerw@mail.ru>
2025-05-11 15:03:35 +0300

    remove test.py

--- Module_15_OOP_Debug/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-count = 0
-for line in open('Module_15_OOP_Debug/data/res_balls.txt','r',encoding='cp1251'):
-    points = int(line.split()[-1])
-    if points < 3:
-        count += 1
