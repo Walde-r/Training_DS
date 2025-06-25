7b1a43da905e77c14b21ef294ee79d13d1424969
Walde_r <washerw@mail.ru>
2025-06-25 10:40:09 +0300

    add EDA

--- Module_14_ClearData/test.py
+++ Module_14_ClearData/test.py
@@ -1,7 +1,5 @@
-import numpy as np
-
-
-l_data = [1.331587, np.nan,  1.000000, 0.265512, -0.174600, 0.128564, -0.965066] 
-
-
-print(np.median(l_data))
+import PySimpleGUI as sg
+layout = [[sg.Text("Привет, мир!")], [sg.Button("OK")]]
+window = sg.Window("Пример PySimpleGUI", layout)
+event, values = window.read()
+window.close()
